05390ccd128a98e3fc61eed1b2c1173f4e88b4e0
Justine Gartner <GartnerJustine@gmail.com>
2015-12-19 16:22:59 -0500

    Successfully pulling and parsing Giphy api

--- unit-3-final-app-assessment/C4QCatFactsDetailViewController.m
+++ unit-3-final-app-assessment/C4QCatFactsDetailViewController.m
@@ -6,13 +6,16 @@
 //  Copyright © 2015 Michael Kavouras. All rights reserved.
 //
 
+#import <AFNetworking/AFNetworking.h>
 #import "C4QCatFactsDetailViewController.h"
 
-#define CAT_GIF_URL @"http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+#define CAT_GIF_URL @"http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
 
 @interface C4QCatFactsDetailViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *catFactLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *giphyImageView;
+
+@property (nonatomic) NSMutableArray *giphyImages;
 
 @end
 
@@ -21,7 +24,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self fetchGiphyImages];
+    
     self.catFactLabel.text = self.catFact;
+    
+    self.giphyImages = [[NSMutableArray alloc] init];
+    
+    
+}
+
+- (void) fetchGiphyImages {
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    [manager GET:CAT_GIF_URL
+      parameters:nil
+        progress:nil
+         success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+             
+             NSArray *results = responseObject[@"data"];
+             
+             for (NSDictionary *result in results) {
+                 
+                 NSString *imageURLStr = result[@"images"][@"original_still"][@"url"];
+                 
+                 [self.giphyImages addObject:imageURLStr];
+             }
+        
+         }
+     
+         failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+             
+             NSLog( @"%@", error.userInfo);
+        
+         }];
 }
 
 @end
